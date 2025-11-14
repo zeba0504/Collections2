238d8bf50889495614da4482dcab183e7e6921d9
Zeba <saidzodazebida@gmail.com>
2025-11-14 17:59:25 +0300

    Filter

--- app/src/main/java/com/example/collections2/MainActivity.kt
+++ app/src/main/java/com/example/collections2/MainActivity.kt
@@ -119,7 +119,62 @@ fun exampleMap(){
     }
 }
 
+fun exampleFilter(){
+    val cookies = listOf(
+        Cookie(
+            name = "Chocolate Chip",
+            softBaked = false,
+            hasFilling = false,
+            price = 1.69
+        ),
+        Cookie(
+            name = "Banana Walnut",
+            softBaked = true,
+            hasFilling = false,
+            price = 1.49
+        ),
+        Cookie(
+            name = "Vanilla Creme",
+            softBaked = false,
+            hasFilling = true,
+            price = 1.59
+        ),
+        Cookie(
+            name = "Chocolate Peanut Butter",
+            softBaked = false,
+            hasFilling = true,
+            price = 1.49
+        ),
+        Cookie(
+            name = "Snickerdoodle",
+            softBaked = true,
+            hasFilling = false,
+            price = 1.39
+        ),
+        Cookie(
+            name = "Blueberry Tart",
+            softBaked = true,
+            hasFilling = true,
+            price = 1.79
+        ),
+        Cookie(
+            name = "Sugar and Sprinkles",
+            softBaked = false,
+            hasFilling = false,
+            price = 1.39
+        )
+    )
+    val softBakedMenu = cookies.filter {
+        it.softBaked
+    }
+    println("Soft cookies:")
+    softBakedMenu.forEach {
+        println("${it.name} - $${it.price}")
+    }
+}
+
 fun main(){
     exampleForEach()
     exampleMap()
+    exampleFilter()
 }
