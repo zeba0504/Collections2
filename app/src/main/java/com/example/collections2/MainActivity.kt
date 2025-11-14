88ceb92b02416c3993e9b409147ad57373cb45c0
Zeba <saidzodazebida@gmail.com>
2025-11-14 18:05:55 +0300

    Fold

--- app/src/main/java/com/example/collections2/MainActivity.kt
+++ app/src/main/java/com/example/collections2/MainActivity.kt
@@ -239,10 +239,63 @@ fun exampleGroupBy(){
     }
 }
 
+fun exampleFold(){
+    val cookies = listOf(
+        Cookie(
+            name = "Chocolate Chip",
+            softBaked = false,
+            hasFilling = false,
+            price = 1.69
+        ),
+        Cookie(
+            name = "Banana Walnut",
+            softBaked = true,
+            hasFilling = false,
+            price = 1.49
+        ),
+        Cookie(
+            name = "Vanilla Creme",
+            softBaked = false,
+            hasFilling = true,
+            price = 1.59
+        ),
+        Cookie(
+            name = "Chocolate Peanut Butter",
+            softBaked = false,
+            hasFilling = true,
+            price = 1.49
+        ),
+        Cookie(
+            name = "Snickerdoodle",
+            softBaked = true,
+            hasFilling = false,
+            price = 1.39
+        ),
+        Cookie(
+            name = "Blueberry Tart",
+            softBaked = true,
+            hasFilling = true,
+            price = 1.79
+        ),
+        Cookie(
+            name = "Sugar and Sprinkles",
+            softBaked = false,
+            hasFilling = false,
+            price = 1.39
+        )
+    )
+    val totalPrice = cookies.fold(0.0) {total, cookie ->
+        total + cookie.price
+    }
+    println("Total price: $${totalPrice}")
+
+}
+
 fun main(){
     exampleForEach()
     exampleMap()
     exampleFilter()
     exampleGroupBy()
+    exampleFold()
 
 }
