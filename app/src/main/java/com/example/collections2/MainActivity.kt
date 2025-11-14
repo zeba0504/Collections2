10629a8ce9d1b58bec3f49c995a53da6462e85bb
Zeba <saidzodazebida@gmail.com>
2025-11-14 17:56:10 +0300

    Map

--- app/src/main/java/com/example/collections2/MainActivity.kt
+++ app/src/main/java/com/example/collections2/MainActivity.kt
@@ -51,13 +51,11 @@ fun exampleForEach() {
             price = 1.39
         )
     )
-    fun main() {
+
         cookies.forEach {
             println("Menu item: $it")
         }
-    }
-
-    cookies.forEach {
+        cookies.forEach {
         println("Menu item: $it.name")
     }
 
@@ -66,6 +64,62 @@ fun exampleForEach() {
     }
 }
 
+fun exampleMap(){
+    val cookies = listOf(
+        Cookie(
+            name = "Chocolate Chip",
+            softBaked = false,
+            hasFilling = false,
+            price = 1.69
+        ),
+        Cookie(
+            name = "Banana Walnut",
+            softBaked = true,
+            hasFilling = false,
+            price = 1.49
+        ),
+        Cookie(
+            name = "Vanilla Creme",
+            softBaked = false,
+            hasFilling = true,
+            price = 1.59
+        ),
+        Cookie(
+            name = "Chocolate Peanut Butter",
+            softBaked = false,
+            hasFilling = true,
+            price = 1.49
+        ),
+        Cookie(
+            name = "Snickerdoodle",
+            softBaked = true,
+            hasFilling = false,
+            price = 1.39
+        ),
+        Cookie(
+            name = "Blueberry Tart",
+            softBaked = true,
+            hasFilling = true,
+            price = 1.79
+        ),
+        Cookie(
+            name = "Sugar and Sprinkles",
+            softBaked = false,
+            hasFilling = false,
+            price = 1.39
+        )
+    )
+
+    val fullMenu = cookies.map {
+        "${it.name} - $${it.price}"
+    }
+    println("Full menu:")
+    fullMenu.forEach {
+        println(it)
+    }
+}
+
 fun main(){
     exampleForEach()
+    exampleMap()
 }
