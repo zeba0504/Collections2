1dec69fd7c9d78df78a7b0f3a9b381ad23586ef6
Zeba <saidzodazebida@gmail.com>
2025-11-14 17:49:41 +0300

    ForEach

--- app/src/main/java/com/example/collections2/MainActivity.kt
+++ app/src/main/java/com/example/collections2/MainActivity.kt
@@ -1,47 +1,71 @@
 package com.example.collections2
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.collections2.ui.theme.Collections2Theme
-
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Collections2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+class Cookie(
+    val name: String,
+    val softBaked: Boolean,
+    val hasFilling: Boolean,
+    val price: Double
+)
+fun exampleForEach() {
+    val cookies = listOf(
+        Cookie(
+            name = "Chocolate Chip",
+            softBaked = false,
+            hasFilling = false,
+            price = 1.69
+        ),
+        Cookie(
+            name = "Banana Walnut",
+            softBaked = true,
+            hasFilling = false,
+            price = 1.49
+        ),
+        Cookie(
+            name = "Vanilla Creme",
+            softBaked = false,
+            hasFilling = true,
+            price = 1.59
+        ),
+        Cookie(
+            name = "Chocolate Peanut Butter",
+            softBaked = false,
+            hasFilling = true,
+            price = 1.49
+        ),
+        Cookie(
+            name = "Snickerdoodle",
+            softBaked = true,
+            hasFilling = false,
+            price = 1.39
+        ),
+        Cookie(
+            name = "Blueberry Tart",
+            softBaked = true,
+            hasFilling = true,
+            price = 1.79
+        ),
+        Cookie(
+            name = "Sugar and Sprinkles",
+            softBaked = false,
+            hasFilling = false,
+            price = 1.39
+        )
+    )
+    fun main() {
+        cookies.forEach {
+            println("Menu item: $it")
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Collections2Theme {
-        Greeting("Android")
+    cookies.forEach {
+        println("Menu item: $it.name")
     }
+
+    cookies.forEach {
+        println("Menu item: ${it.name}")
+    }
+}
+
+fun main(){
+    exampleForEach()
 }
