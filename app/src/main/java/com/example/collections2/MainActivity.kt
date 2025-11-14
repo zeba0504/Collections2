4216b877ab787ac65063aee2fb97d84b68bad862
Zeba <saidzodazebida@gmail.com>
2025-11-14 18:10:42 +0300

    sortedBy

--- app/src/main/java/com/example/collections2/MainActivity.kt
+++ app/src/main/java/com/example/collections2/MainActivity.kt
@@ -288,7 +288,60 @@ fun exampleFold(){
         total + cookie.price
     }
     println("Total price: $${totalPrice}")
+}
 
+fun examlpeSortedBy(){
+    val cookies = listOf(
+        Cookie(
+            name = "Chocolate Chip",
+            softBaked = false,
+            hasFilling = false,
+            price = 1.69
+        ),
+        Cookie(
+            name = "Banana Walnut",
+            softBaked = true,
+            hasFilling = false,
+            price = 1.49
+        ),
+        Cookie(
+            name = "Vanilla Creme",
+            softBaked = false,
+            hasFilling = true,
+            price = 1.59
+        ),
+        Cookie(
+            name = "Chocolate Peanut Butter",
+            softBaked = false,
+            hasFilling = true,
+            price = 1.49
+        ),
+        Cookie(
+            name = "Snickerdoodle",
+            softBaked = true,
+            hasFilling = false,
+            price = 1.39
+        ),
+        Cookie(
+            name = "Blueberry Tart",
+            softBaked = true,
+            hasFilling = true,
+            price = 1.79
+        ),
+        Cookie(
+            name = "Sugar and Sprinkles",
+            softBaked = false,
+            hasFilling = false,
+            price = 1.39
+        )
+    )
+    val alphabeticalMenu = cookies.sortedBy {
+        it.name
+    }
+    println("Alphabetical menu:")
+    alphabeticalMenu.forEach {
+        println(it.name)
+    }
 }
 
 fun main(){
@@ -297,5 +350,5 @@ fun main(){
     exampleFilter()
     exampleGroupBy()
     exampleFold()
-
+    examlpeSortedBy()
 }
